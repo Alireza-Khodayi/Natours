e64a2597110e588e055bf87feb18731524279aec
Alireza Khodayi <warmanealireza1997@gmail.com>
2024-02-22 16:21:40 +0330

    Basic Authentication Functions and Model implemented

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: [8, 'Password must be at least 8 characters long!'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password!'],
+    validate: {
+      //This only works on CREATE and SAVE!!
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Passwords do not match!',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only runs this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
