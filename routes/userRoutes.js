bb2719a93c8c60213b0508c28b772d64f1ddbf2a
Alireza Khodayi <warmanealireza1997@gmail.com>
2024-02-26 17:31:53 +0330

    Delete Current User Implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
   updateMe,
   updateUser,
   deleteUser,
+  deleteMe,
 } = require('../controllers/userController');
 const {
   signUp,
@@ -25,7 +26,10 @@ router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
+
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
